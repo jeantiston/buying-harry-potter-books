5678eaa77476b4876a1a2bd992a10b27dede6c0f
jeantiston <jean.tiston.work@gmail.com>
2021-06-24 18:58:30 +0800

    Update shoppingUtilities.js

--- shoppingUtilities.js
+++ shoppingUtilities.js
@@ -1,5 +1,11 @@
 const calculateShoppingCart = cart => {
-    
+    var total = 0
+
+    for (var key in cart) {
+        total += cart[key] * 8
+    }
+
+    return total
 }
 
 export default calculateShoppingCart
